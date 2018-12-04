9c785b4de454a17c17b7d5be8530f17a7860e66d
Brendan Hosking <brendan.hosking@csiro.au>
2018-12-04 15:25:16 +1100

    FIX: Chnage dynamodb hash key to "id"

--- modules/beacon-api/dynamodb.tf
+++ modules/beacon-api/dynamodb.tf
@@ -1,11 +1,11 @@
 resource "aws_dynamodb_table" "datasets" {
-  hash_key = "DatasetID"
+  hash_key = "id"
   name = "Datasets"
   read_capacity = 5
   write_capacity = 1
 
   attribute {
-    name = "DatasetID"
+    name = "id"
     type = "S"
   }
 }
