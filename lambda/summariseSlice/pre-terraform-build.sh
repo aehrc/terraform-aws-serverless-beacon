a24d86c7f9cba0bf981be8f166a53682f480b7cb
Brendan Hosking <brendan.hosking@csiro.au>
2019-02-18 11:28:26 +1100

    FEAT: Enable automatic creation of zip files

--- lambda/summariseSlice/pre-terraform-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-FUNCTION_NAME="summariseSlice"
-
-zip -FS $FUNCTION_NAME.zip lambda_function.py bcftools
--- lambda/summariseVcf/pre-terraform-build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-FUNCTION_NAME="summariseVcf"
-
-zip -FS $FUNCTION_NAME.zip lambda_function.py bcftools chrom_matching.py lambda_utils.py tabix
