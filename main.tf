86bf993cd9afa82eaecbc79d1d2f74c39e14c7d2
Brendan Hosking <brendan.hosking@csiro.au>
2021-11-01 03:28:58 +0000

    STYLE: Move s3 bucket to own file

--- main.tf
+++ main.tf
@@ -265,12 +265,3 @@ module "lambda-performQuery" {
   source_path = "${path.module}/lambda/performQuery"
   tags = var.common-tags
 }
-
-#
-# S3 bucket for persisted vcf summaries (duplicate variant search feature)
-#
-resource "aws_s3_bucket" "s3-summaries-bucket" {
-  bucket_prefix = var.summaries-bucket-prefix
-  acl = "private"
-  tags = var.common-tags
-}
