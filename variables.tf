c4654dff391023603b5db123867d89b586eba211
Anuradha <anuradhawick@gmail.com>
2022-08-18 23:25:41 +0000

    update README and refactoring code

--- variables.tf
+++ variables.tf
@@ -1,43 +1,47 @@
 variable "beacon-id" {
-  type = string
-  description = "Unique identifier of the beacon. Use reverse domain name notation."
+  type         = string
+  description  = "Unique identifier of the beacon. Use reverse domain name notation."
+  default      = "sbeacon.csiro.au"
 }
 
 variable "beacon-name" {
-  type = string
-  description = "Human readable name of the beacon."
+  type         = string
+  description  = "Human readable name of the beacon."
+  default      = "CSIRO Serverless Beacon"
 }
 
 variable "organisation-id" {
-  type = string
-  description = "Unique identifier of the organization providing the beacon."
+  type         = string
+  description  = "Unique identifier of the organization providing the beacon."
+  default      = "CSIRO"
 }
 
 variable "organisation-name" {
-  type = string
-  description = "Name of the organization providing the beacon."
+  type         = string
+  description  = "Name of the organization providing the beacon."
+  default      = "CSIRO"
 }
 
 variable "common-tags" {
-  type = map(string)
-  description = "A set of tags to attach to every created resource."
-  default = {}
+  type         = map(string)
+  description  = "A set of tags to attach to every created resource."
+  default      = {}
 }
 
 variable "variants-bucket-prefix" {
-    type        = string
-    description = "S3 bucket for storing vcf summaries used for duplicate variant searches"
-    default     = ""
+  type         = string
+  description  = "S3 bucket for storing vcf summaries used for duplicate variant searches"
+  default      = "sbeacon-variants-"
 }
 
 variable "metadata-bucket-prefix" {
-  type        = string
-  description = "S3 bucket for storing metadata"
-  default     = ""
+  type         = string
+  description  = "S3 bucket for storing metadata"
+  default      = "sbeacon-metadata-"
 }
 
 variable "lambda-layers-bucket-prefix" {
-  type        = string
-  description = "S3 bucket for storing lambda layers"
-  default     = ""
+  type         = string
+  description  = "S3 bucket for storing lambda layers"
+  default      = "sbeacon-lambda-layers-"
 }
