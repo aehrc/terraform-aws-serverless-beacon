48a0bd3ae2be1a2a47c4c40a5d4cd0f46ae7280b
Anuradha <anuradhawick@gmail.com>
2023-03-10 03:12:51 +0000

    FIX - variable name, and code formatting

--- shared_resources/athena/biosample.py
+++ shared_resources/athena/biosample.py
@@ -44,33 +44,32 @@ class Biosample(jsons.JsonSerializable, AthenaModel):
         'notes'
     ]
 
-
     def __init__(
-                self,
-                *,
-                id='',
-                datasetId='',
-                cohortId='',
-                individualId='',
-                biosampleStatus={},
-                collectionDate=[],
-                collectionMoment=[],
-                diagnosticMarkers=[],
-                histologicalDiagnosis=[],
-                measurements=[],
-                obtentionProcedure=[],
-                pathologicalStage=[],
-                pathologicalTnmFinding=[],
-                phenotypicFeatures=[],
-                sampleOriginDetail=[],
-                sampleOriginType=[],
-                sampleProcessing=[],
-                sampleStorage=[],
-                tumorGrade=[],
-                tumorProgression=[],
-                info=[],
-                notes=[]
-            ):
+        self,
+        *,
+        id='',
+        datasetId='',
+        cohortId='',
+        individualId='',
+        biosampleStatus={},
+        collectionDate=[],
+        collectionMoment=[],
+        diagnosticMarkers=[],
+        histologicalDiagnosis=[],
+        measurements=[],
+        obtentionProcedure=[],
+        pathologicalStage=[],
+        pathologicalTnmFinding=[],
+        phenotypicFeatures=[],
+        sampleOriginDetail=[],
+        sampleOriginType=[],
+        sampleProcessing=[],
+        sampleStorage=[],
+        tumorGrade=[],
+        tumorProgression=[],
+        info=[],
+        notes=[]
+    ):
         self.id = id
         self._datasetId = datasetId
         self._cohortId = cohortId
@@ -94,43 +93,44 @@ class Biosample(jsons.JsonSerializable, AthenaModel):
         self.info = info
         self.notes = notes
 
-
     def __eq__(self, other):
         return self.id == other.id
-
 
     @classmethod
     def upload_array(cls, array):
         if len(array) == 0:
             return
-        header = 'struct<' + ','.join([f'{col.lower()}:string' for col in cls._table_columns]) + '>'
-        bloom_filter_columns = list(map(lambda x: x.lower(), cls._table_columns))
+        header = 'struct<' + \
+            ','.join(
+                [f'{col.lower()}:string' for col in cls._table_columns]) + '>'
+        bloom_filter_columns = list(
+            map(lambda x: x.lower(), cls._table_columns))
         key = f'{array[0]._datasetId}-biosamples'
-        
+
         with sopen(f's3://{ATHENA_METADATA_BUCKET}/biosamples/{key}', 'wb') as s3file:
             with pyorc.Writer(
-                s3file, 
-                header, 
-                compression=pyorc.CompressionKind.SNAPPY, 
-                compression_strategy=pyorc.CompressionStrategy.COMPRESSION,
-                bloom_filter_columns=bloom_filter_columns) as writer:
+                    s3file,
+                    header,
+                    compression=pyorc.CompressionKind.SNAPPY,
+                    compression_strategy=pyorc.CompressionStrategy.COMPRESSION,
+                    bloom_filter_columns=bloom_filter_columns) as writer:
                 for biosample in array:
                     row = tuple(
-                        biosample.__dict__[k] 
+                        biosample.__dict__[k]
                         if type(biosample.__dict__[k]) == str
                         else json.dumps(biosample.__dict__[k])
                         for k in cls._table_columns
                     )
                     writer.write(row)
-        
+
         header = 'struct<kind:string,id:string,term:string,label:string,type:string>'
         with sopen(f's3://{ATHENA_METADATA_BUCKET}/terms-cache/biosamples-{key}', 'wb') as s3file:
             with pyorc.Writer(
-                s3file, 
-                header, 
-                compression=pyorc.CompressionKind.SNAPPY, 
-                compression_strategy=pyorc.CompressionStrategy.COMPRESSION) as writer:
-                
+                    s3file,
+                    header,
+                    compression=pyorc.CompressionKind.SNAPPY,
+                    compression_strategy=pyorc.CompressionStrategy.COMPRESSION) as writer:
+
                 for biosample in array:
                     for term, label, typ in extract_terms([jsons.dump(biosample)]):
                         row = ('biosamples', biosample.id, term, label, typ)
