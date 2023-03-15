187dee0d7bb6390d041ca9a44d5db5cbc81e3272
Anuradha <anuradhawick@gmail.com>
2023-03-15 06:01:13 +0000

    Update entity search conditions, TODO - update with passing Class as id_type and default scope for neater SQL creation

--- shared_resources/athena/filter_functions.py
+++ shared_resources/athena/filter_functions.py
@@ -1,15 +1,20 @@
 import os
+from typing import List, Union
 
 from dynamodb.ontologies import Descendants, Anscestors
+from apiutils.requests import OntologyFilter, AlphanumericFilter, CustomFilter, Similarity, Operator
 from .analysis import Analysis
 from .biosample import Biosample
 from .individual import Individual
 from .cohort import Cohort
 from .dataset import Dataset
 from .run import Run
+from .common import AthenaModel
+
 
 ATHENA_TERMS_INDEX_TABLE = os.environ['ATHENA_TERMS_INDEX_TABLE']
 ATHENA_RELATIONS_TABLE = os.environ['ATHENA_RELATIONS_TABLE']
+
 
 queried_athena_models = {a.__name__: a for a in [
     Analysis, Biosample, Individual, Cohort, Dataset, Run]}
@@ -70,8 +75,8 @@ def _get_term_descendants(term):
     return terms
 
 
-def entity_search_conditions(filters, id_type, default_scope, id_modifier='id', with_where=True):
-    assert id_type in id_type_string_to_class.keys(), "id_type must be recognised"
+def entity_search_conditions(filters: List[Union[OntologyFilter, AlphanumericFilter, CustomFilter]], id_type: str, default_scope, id_modifier='id', with_where=True):
+    # assert id_type in id_type_string_to_class.keys(), "id_type must be recognised"
     id_class = id_type_string_to_class[id_type]
 
     # arrays to gradually form the SQL expression
@@ -86,53 +91,51 @@ def entity_search_conditions(filters, id_type, default_scope, id_modifier='id', 
         # terms relating directly to the respective table columns
         # or terms relating to tables linked to the respective table
 
-        assert "id" in f.keys(), "filter without 'id' specified"
-        f_id = f['id'].split(".")
+        # assert "id" in f.keys(), "filter without 'id' specified"
+        f_id = f.id.split(".")
 
         # check to see if there is any relevent field of the referred id_type
         if len(f_id) == 1 and f_id[0] in id_class._table_columns:
-            operator, value = _get_comparrison_fragment(f)
+            operator = '!=' if f.operator == Operator.NOT else f.operator
             outer_constraints.append("{} {} ?".format(f_id[0], operator))
-            outer_execution_parameters.append(str(value))
-        else:
-            # check to see if it matches the fields of any linked class
-            if len(f_id) == 2 and f_id[0] in queried_athena_models.keys() and f_id[1] in queried_athena_models[f_id[0]]._table_columns:
-                joined_class = queried_athena_models[f_id[0]]
-                operator, value = _get_comparrison_fragment(f)
-                comparrison = "{} {} ?".format(f_id[1], operator)
-                join_execution_parameters.append(str(value))
-                group = class_to_id_type_string[joined_class]
-                join_constraints.append(
-                    f''' SELECT RI.{type_relations_table_id[id_type]} FROM "{ATHENA_RELATIONS_TABLE}" RI JOIN "{joined_class._table_name}" TI ON RI.{type_relations_table_id[group]} = TI.id where TI.{comparrison} ''')
-            # if none of these two cases match, assume it is an ontology term
-            else:
-                includeDescendantTerms = f.get("includeDescendantTerms", True)
-                # if descendantTerms is false, then similarity measures dont really make sense...
-                if includeDescendantTerms:
-                    # process inclusion of term descendants dependant on 'similarity'
-                    similarity = f.get("similarity", "high")
-                    if similarity == "high":
-                        expanded_terms = _get_term_descendants(f['id'])
-                    else:
-                        # NOTE: this simplistic similarity method not nessisarily efficient or nessisarily desirable
-                        ancestors = _get_term_ancestors(f['id'])
-                        ancestor_descendants = sorted(
-                            [_get_term_descendants(a) for a in ancestors], key=len)
-                        if similarity == "medium":
-                            # all terms which have an ancestor half way up
-                            expanded_terms = ancestor_descendants[len(
-                                ancestor_descendants)//2]
-                        elif similarity == "low":
-                            # all terms which have any ancestor in common
-                            expanded_terms = ancestor_descendants[-1]
+            outer_execution_parameters.append(str(f.value))
+
+        elif len(f_id) == 2 and f_id[0] in queried_athena_models.keys() and f_id[1] in queried_athena_models[f_id[0]]._table_columns:
+            joined_class = queried_athena_models[f_id[0]]
+            operator = '!=' if f.operator == Operator.NOT else f.operator
+            comparrison = "{} {} ?".format(f_id[1], operator)
+            join_execution_parameters.append(str(f.value))
+            group = class_to_id_type_string[joined_class]
+            join_constraints.append(
+                f''' SELECT RI.{type_relations_table_id[id_type]} FROM "{ATHENA_RELATIONS_TABLE}" RI JOIN "{joined_class._table_name}" TI ON RI.{type_relations_table_id[group]} = TI.id where TI.{comparrison} ''')
+
+        if isinstance(f, OntologyFilter):
+            # if descendantTerms is false, then similarity measures dont really make sense...
+            if f.include_descendant_terms:
+                # process inclusion of term descendants dependant on 'similarity'
+                if f.similarity == Similarity.HIGH:
+                    expanded_terms = _get_term_descendants(f.id)
                 else:
-                    expanded_terms = set([f['id']])
-                join_execution_parameters += [str(a) for a in expanded_terms]
-                expanded_terms = " , ".join(["?" for a in expanded_terms])
-                # process scope clarification if specified different
-                group = f.get("scope", default_scope)
-                join_constraints.append(
-                    f''' SELECT RI.{type_relations_table_id[id_type]} FROM "{ATHENA_RELATIONS_TABLE}" RI JOIN "{ATHENA_TERMS_INDEX_TABLE}" TI ON RI.{type_relations_table_id[group]} = TI.id where TI.term IN ({expanded_terms}) ''')
+                    # NOTE: this simplistic similarity method not nessisarily efficient or nessisarily desirable
+                    ancestors = _get_term_ancestors(f.id)
+                    ancestor_descendants = sorted(
+                        [_get_term_descendants(a) for a in ancestors], key=len)
+                    if f.similarity == Similarity.MEDIUM:
+                        # all terms which have an ancestor half way up
+                        expanded_terms = ancestor_descendants[len(
+                            ancestor_descendants)//2]
+                    elif f.similarity == Similarity.LOW:
+                        # all terms which have any ancestor in common
+                        expanded_terms = ancestor_descendants[-1]
+            else:
+                expanded_terms = {f.id}
+
+            join_execution_parameters += [str(a) for a in expanded_terms]
+            expanded_terms = " , ".join(["?" for a in expanded_terms])
+            # process scope clarification if specified different
+            group = f.scope or default_scope
+            join_constraints.append(
+                f''' SELECT RI.{type_relations_table_id[id_type]} FROM "{ATHENA_RELATIONS_TABLE}" RI JOIN "{ATHENA_TERMS_INDEX_TABLE}" TI ON RI.{type_relations_table_id[group]} = TI.id where TI.term IN ({expanded_terms}) ''')
 
     # format fragments together to form coherent SQL expression
     join_constraints = " INTERSECT ".join(join_constraints)
