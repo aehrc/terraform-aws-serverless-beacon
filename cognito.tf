6869924b20e934db302bea3d193161545eea34c3
NickEdwards7502 <nickedwards7502@gmail.com>
2024-09-26 16:24:41 +1000

    DEV: Update cognito user pool, groups, and group asssignments
    * Add additional attributes to user pool schema
    
    * Create user group and associated roles and role policies for admins
    
    * Add name attributes to guest and admin users
    
    * Create group assignment for admin actions

--- cognito.tf
+++ cognito.tf
@@ -30,8 +30,11 @@ resource "aws_cognito_user_pool" "BeaconUserPool" {
   }
 
   schema {
-    name                = "terraform"
-    attribute_data_type = "Boolean"
+    name                     = "terraform"
+    attribute_data_type      = "Boolean"
+    developer_only_attribute = false
+    mutable                  = false
+    required                 = false
   }
 }
 
@@ -50,6 +53,78 @@ resource "aws_cognito_user_pool_client" "BeaconUserPool-client" {
 # 
 # groups
 # 
+resource "aws_cognito_user_group" "admin-group" {
+  name         = "admin-group"
+  user_pool_id = aws_cognito_user_pool.BeaconUserPool.id
+  description  = "Group of users who can has admin privileges"
+  role_arn     = aws_iam_role.admin-group-role.arn
+}
+
+data "aws_iam_policy_document" "admin-group-assume-role-policy" {
+  statement {
+    effect = "Deny"
+
+    principals {
+      type        = "Federated"
+      identifiers = ["cognito-identity.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "cognito-identity.amazonaws.com:aud"
+      values   = ["us-east-1:12345678-dead-beef-cafe-123456790ab"]
+    }
+
+    condition {
+      test     = "ForAnyValue:StringLike"
+      variable = "cognito-identity.amazonaws.com:amr"
+      values   = ["authenticated"]
+    }
+  }
+  statement {
+    effect = "Deny"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:sts::${data.aws_caller_identity.this.account_id}:assumed-role/admin/admin"
+      ]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "admin-group-role" {
+  name               = "admin-group-role"
+  assume_role_policy = data.aws_iam_policy_document.admin-group-assume-role-policy.json
+}
+
+data "aws_iam_policy_document" "admin-group-role-policy" {
+  statement {
+    actions = [
+      "cognito-idp:*"
+    ]
+    resources = [
+      aws_cognito_user_pool.BeaconUserPool.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "admin-group-role-policy" {
+  name        = "admin-group-role-policy"
+  description = "admin group permissions"
+  policy      = data.aws_iam_policy_document.admin-group-role-policy.json
+
+}
+
+resource "aws_iam_role_policy_attachment" "admin-group-role-policy-attachment" {
+  role       = aws_iam_role.admin-group-role.name
+  policy_arn = aws_iam_policy.admin-group-role-policy.arn
+}
+
 resource "aws_cognito_user_group" "record-access" {
   name         = "record-access-user-group"
   user_pool_id = aws_cognito_user_pool.BeaconUserPool.id
@@ -80,6 +155,8 @@ resource "aws_cognito_user" "guest" {
     terraform      = true
     email          = var.beacon-guest-username
     email_verified = true
+    given_name     = "Guest"
+    family_name    = "Guest"
   }
 }
 
@@ -90,6 +167,8 @@ resource "aws_cognito_user" "admin" {
 
   attributes = {
     terraform      = true
+    given_name     = "Admin"
+    family_name    = "Admin"
     email          = var.beacon-admin-username
     email_verified = true
   }
@@ -99,6 +178,12 @@ resource "aws_cognito_user" "admin" {
 # group assignments
 # 
 # admin
+resource "aws_cognito_user_in_group" "admin-admin-access" {
+  user_pool_id = aws_cognito_user_pool.BeaconUserPool.id
+  group_name   = aws_cognito_user_group.admin-group.name
+  username     = aws_cognito_user.admin.username
+}
+
 resource "aws_cognito_user_in_group" "admin-record-access" {
   user_pool_id = aws_cognito_user_pool.BeaconUserPool.id
   group_name   = aws_cognito_user_group.record-access.name
