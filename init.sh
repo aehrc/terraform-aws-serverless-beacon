ae757c036bbd7852c9bbe65d19203292c06ad9e2
NickEdwards7502 <nickedwards7502@gmail.com>
2024-09-26 16:29:05 +1000

    REFACTOR: Remove c++ runtime library installation process
    * No longer required as c++ is no longer being used in lambda functions

--- init.sh
+++ init.sh
@@ -10,61 +10,15 @@ set -ex
 REPOSITORY_DIRECTORY="${PWD}"
 LIBRARIES="${REPOSITORY_DIRECTORY}/libraries"
 SOURCE="${LIBRARIES}/source"
-INCLUDE="${LIBRARIES}/include"
 
-if [ -z "$1" ]
-  then
-    CMAKE_FIRST_LINE=""
-  else
-    CMAKE_FIRST_LINE="add_compile_options(${@})"
-fi
-
-# Update CMakeLists.txt for each C++ lambda function
-for cmakefile in "${REPOSITORY_DIRECTORY}/lambda/"*/source/CMakeLists.txt; do
-  sed -i "1s/^.*/${CMAKE_FIRST_LINE}/" "${cmakefile}"
-done
-
-# Clean ~/varscot-libraries
+# Clean sbeacon-libraries
 if [ -d "${LIBRARIES}" ]
   then
     rm -rf "${LIBRARIES}"
 fi
 
 mkdir "${LIBRARIES}"
-mkdir "${INCLUDE}"
 mkdir "${SOURCE}"
-
-# 
-# Runtime libraries for CPP development
-# 
-
-# Build the C++ AWS SDK libraries
-cd "${SOURCE}"
-# Tag to avoid build error introduced in 1.9.49
-git clone https://github.com/aws/aws-sdk-cpp.git --depth=1 --recursive --branch 1.11.63
-cd aws-sdk-cpp
-sed -i "1s/^/${CMAKE_FIRST_LINE}\n/" CMakeLists.txt
-mkdir build
-cd build
-cmake .. -DBUILD_ONLY="dynamodb;s3;sns" \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DCUSTOM_MEMORY_MANAGEMENT=OFF \
-  -DCMAKE_INSTALL_PREFIX="${INCLUDE}" \
-  -DENABLE_UNITY_BUILD=ON
-make install
-
-# Build the lambda runtime libraries
-cd ${SOURCE}
-git clone https://github.com/awslabs/aws-lambda-cpp-runtime.git --depth=1
-cd aws-lambda-cpp-runtime
-sed -i "1s/^/${CMAKE_FIRST_LINE}\n/" CMakeLists.txt
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DCMAKE_INSTALL_PREFIX="${INCLUDE}"
-make install
 
 #
 # building lambda layers
@@ -97,8 +51,8 @@ pip install jsons==1.6.3 --target layers/python_libraries/python
 pip install jsonschema==4.18.0 --target layers/python_libraries/python
 pip install pydantic==2.0.2 --target layers/python_libraries/python
 pip install pyhumps==3.8.0 --target layers/python_libraries/python
-pip install pynamodb==5.5.0 --target layers/python_libraries/python
-pip install pyorc==0.8.0 --target layers/python_libraries/python
+pip install pynamodb==6.0.0 --target layers/python_libraries/python
+pip install pyorc==0.9.0 --target layers/python_libraries/python
 pip install requests==2.31.0 --target layers/python_libraries/python
-pip install smart_open==6.3.0 --target layers/python_libraries/python
+pip install smart_open==7.0.4 --target layers/python_libraries/python
 pip install strenum==0.4.15 --target layers/python_libraries/python
