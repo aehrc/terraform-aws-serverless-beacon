5ccc9e9b4ed55b66b277578ee3d841794acbd9e6
NickEdwards7502 <nickedwards7502@gmail.com>
2024-09-26 16:30:09 +1000

    REFACTOR: Remove summarise & duplicate variant search lambda functions

--- lambda.tf
+++ lambda.tf
@@ -1,48 +1,4 @@
 #
-# summariseDataset Lambda Function
-#
-resource "aws_lambda_permission" "SNSSummariseDataset" {
-  statement_id  = "AllowSNSSummariseDatasetInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda-summariseDataset.lambda_function_arn
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.summariseDataset.arn
-}
-
-#
-# summariseVcf Lambda Function
-#
-resource "aws_lambda_permission" "SNSSummariseVcf" {
-  statement_id  = "AllowSNSSummariseVcfInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda-summariseVcf.lambda_function_arn
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.summariseVcf.arn
-}
-
-#
-# summariseSlice Lambda Function
-#
-resource "aws_lambda_permission" "SNSSummariseSlice" {
-  statement_id  = "AllowSNSSummariseSliceInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda-summariseSlice.lambda_function_arn
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.summariseSlice.arn
-}
-
-#
-# duplicateVariantSearch Lambda Function
-#
-resource "aws_lambda_permission" "SNSduplicateVariantSearch" {
-  statement_id  = "AllowSNSduplicateVariantSearchInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = module.lambda-duplicateVariantSearch.lambda_function_arn
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.duplicateVariantSearch.arn
-}
-
-#
 # splitQuery Lambda Function
 #
 resource "aws_lambda_permission" "SNSsplitQuery" {
